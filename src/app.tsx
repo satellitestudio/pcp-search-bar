f005e6357fa2278031a85f9fa3b23f9220d78eaf
j8seangel <jseangel@gmail.com>
2019-04-18 11:56:30 +0200

    Basic proyect boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './app.css'
+import Search from './components/search/search'
+
+const App: React.FC = (): React.ReactElement => (
+  <div className="app">
+    <Search />
+  </div>
+)
+
+export default App
