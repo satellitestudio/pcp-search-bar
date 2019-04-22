e3994fe562e10a5db1830eab976da0a2f43d0de3
j8seangel <jseangel@gmail.com>
2019-04-22 18:47:45 +0200

    Highglight first by default

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -3,42 +3,10 @@ import styles from './search.module.css'
 import Downshift, { DownshiftState, StateChangeOptions } from 'downshift'
 import { DataItem } from '../../types/data'
 
-function ArrowIcon({ isOpen }: { isOpen: boolean }) {
-  return (
-    <svg
-      viewBox="0 0 20 20"
-      preserveAspectRatio="none"
-      width={16}
-      fill="transparent"
-      stroke="#979797"
-      strokeWidth="1.1px"
-      transform={isOpen ? 'rotate(180)' : ''}
-    >
-      <path d="M1,6 L10,15 L19,6" />
-    </svg>
-  )
-}
-
-function XIcon() {
-  return (
-    <svg
-      viewBox="0 0 20 20"
-      preserveAspectRatio="none"
-      width={12}
-      fill="transparent"
-      stroke="#979797"
-      strokeWidth="1.1px"
-    >
-      <path d="M1,1 L19,19" />
-      <path d="M19,1 L1,19" />
-    </svg>
-  )
-}
-
 interface SearchProps {
   isOpen?: boolean
   itemToString(obj: DataItem): string
-  onChange(selectedItem: DataItem, downshiftState: DownshiftState<any>): void
+  onChange(selectedItems: DataItem[], downshiftState: DownshiftState<any>): void
   items: DataItem[]
   stateReducer(
     state: DownshiftState<any>,
@@ -55,36 +23,26 @@ const Search: React.FC<SearchProps> = (props) => {
       stateReducer={stateReducer}
       itemToString={itemToString}
       onStateChange={onStateChange}
+      defaultHighlightedIndex={0}
     >
       {(downshift) => {
         const {
           getInputProps,
           getMenuProps,
-          getToggleButtonProps,
           getItemProps,
           isOpen,
-          clearSelection,
           selectedItem,
-          inputValue,
           highlightedIndex,
         } = downshift
         return (
           <div className={styles.searchContainer}>
             <div>
               <input
+                className={styles.searchInput}
                 {...getInputProps({
                   placeholder: 'Start searching',
                 })}
               />
-              {selectedItem ? (
-                <button onClick={() => clearSelection()} aria-label="clear selection">
-                  <XIcon />
-                </button>
-              ) : (
-                <button {...getToggleButtonProps()}>
-                  <ArrowIcon isOpen={isOpen} />
-                </button>
-              )}
             </div>
             {!isOpen ? null : (
               <ul className={styles.optionList} {...getMenuProps()}>
